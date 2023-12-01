a8574f4abf8b8ae696bb44b68688c0ac52c7e11e
John-Sheer <meskojohn@gmail.com>
2023-11-30 16:54:44 -0800

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
-#  Prints the number and the list of its arguments
 if __name__ == "__main__":
     import sys
-    arg = sys.argv
-    size = len(arg) - 1
+    i = len(sys.argv) - 1
 
-    if size > 1:
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
+    else:
+        print("{} arguments:".format(i))
+
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
